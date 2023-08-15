3da4c7431243e9850f3ed376148dbf4a21258135
Aline Jamberci <jambercialine@outlook.com>
2023-08-15 13:16:34 -0300

    Implementados os métodos e programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,40 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		Locale.setDefault(Locale.US);
+		
+		Rectangle rectangle = new Rectangle ();
+		
+		double area, perimeter, diagonal;
+		System.out.println("Enter rectangle width and height");
+		System.out.print("widht: ");
+		rectangle.width = sc.nextDouble();
+		System.out.print("height: ");
+		rectangle.height = sc.nextDouble();
+		
+		System.out.println();
+		
+		area = rectangle.area();
+		System.out.printf("Area: %.2f%n", area);
+		perimeter = rectangle.perimeter();
+		System.out.printf("Perimeter: %.2f%n",  perimeter);
+		diagonal = rectangle.diagonal();
+		System.out.printf("Diagonal: %.2f%n", diagonal);
+		
+		sc.close();
+		
+		
+		
+		
 
 	}
 
